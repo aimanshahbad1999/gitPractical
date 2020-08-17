119ef03f0a92b82b8bf17de6ed48504fa7143767
aiman_cuelogic <aiman.shahbad@cuelogic.com>
2020-08-17 18:35:41 -0400

    Multiplication of two number

--- calculation.py
+++ calculation.py
@@ -2,7 +2,19 @@ def add(a,b):
      c=a+b
      print("Addition of two number: "+str(c))
 
+def substract(a,b):
+     c=a-b
+     print("Substraction of two number: "+str(c))
+
+def multiply(a,b):
+     c=a*b
+     print("Multiplication of two : "+str(c))
+
+
 
 no1=int(intput())
 no2=int(input())
+
 add(no1,no2)
+substract(no1,no2)
+multiply(no1,no2)
