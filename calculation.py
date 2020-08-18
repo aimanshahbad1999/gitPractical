2dca466e0c6c42c04d38beef97503298c48777dc
aiman_cuelogic <aiman.shahbad@cuelogic.com>
2020-08-18 11:50:07 -0400

    Resolve Conflicts

--- calculation.py
+++ calculation.py
@@ -6,16 +6,11 @@ def substract(a,b):
      c=a-b
      print("Substraction of two number: "+str(c))
 
-def multiply(a,b):
-     c=a*b
-     print("Multiplication of two : "+str(c))
-
-
 
 no1=int(intput())
 no2=int(input())
 
 add(no1,no2)
 substract(no1,no2)
-multiply(no1,no2)
+
 
