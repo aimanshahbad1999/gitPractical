c5ded91c81fc950cbcc526456f8d1f99ddabe2ac
aiman_cuelogic <aiman.shahbad@cuelogic.com>
2020-08-18 13:42:37 -0400

    Division of two number

--- calculation.py
+++ calculation.py
@@ -2,7 +2,26 @@ def add(a,b):
      c=a+b
      print("Addition of two number: "+str(c))
 
+def substract(a,b):
+     c=a-b
+     print("Substraction of two number: "+str(c))
+
+
+def multiply(a,b):
+     c=a*b
+     print("Multiplication of two number: "+str(c))
+
+def dev(a,b):
+     c=a/b
+     print("Division of two number:"str(c))
+
+
+
 
 no1=int(intput())
 no2=int(input())
+
 add(no1,no2)
+substract(no1,no2)
+multiply(no1,no2)
+dev(no1,no2)
