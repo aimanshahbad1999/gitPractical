cb90627848a8b1d1cbdd71527553750916df36f7
aiman_cuelogic <aiman.shahbad@cuelogic.com>
2020-08-18 11:59:01 -0400

    QA staging branch changes DONE

--- calculation.py
+++ calculation.py
@@ -1,8 +1,22 @@
 def add(a,b):
      c=a+b
-     print("Addition of two number: "+str(c))
+     if c>0:
+        print("Addition of two number: "+str(c))
+     else:
+        print("Addition of two number: "+str(abs(c)))
+         
+        
 
+def substract(a,b):
+     c=a-b
+     if c>0:
+        print("Substraction of two number: "+str(c))
+     else:
+        print("Substraction of two number: "+str(abs(c)))
+         
 
-no1=int(intput())
-no2=int(input())
+no1=int(input("Enter Value of a: "))
+no2=int(input("Enter Value of b: "))
+
 add(no1,no2)
+substract(no1,no2)
